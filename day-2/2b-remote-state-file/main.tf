65c3aacd6069dc3bcfd221be98263816717634b1
venkat <venkatkumar78651@gmail.com>
2025-03-18 15:06:06 +0530

    the secondcode

--- day-2/2b-remote-state-file/main.tf
+++ day-2/2b-remote-state-file/main.tf
@@ -1,10 +1,4 @@
 provider "azurerm" {
-  subscription_id = "b19e47e5-a91e-485e-885b-5d2471405f35"
-  client_id       = "dd2effbd-e591-4d76-ac94-314a36ab3e3f"
-  client_secret   = "eb645fcc-0397-4996-ad2f-284fc5d31f01"
-  tenant_id       = "a0b3fea9-510d-46b2-a3ea-afb52c7c0c63"
-  features {}
-}
   features {
     
   }
